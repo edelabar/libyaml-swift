f5355727873414e7750945eddffcef36fe3a5377
Niels de Hoog <njdehoog@gmail.com>
2015-07-15 12:49:09 +0200

    Initial commit

---
+++ swift-yaml/SwiftYAML.h
@@ -0,0 +1,17 @@
+//
+//  swift-yaml.h
+//  swift-yaml
+//
+//  Created by Niels de Hoog on 15/07/15.
+//  Copyright © 2015 Invisible Pixel. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for swift-yaml.
+FOUNDATION_EXPORT double swift_yamlVersionNumber;
+
+//! Project version string for swift-yaml.
+FOUNDATION_EXPORT const unsigned char swift_yamlVersionString[];
+
+#import <SwiftYAML/yaml.h>
---
+++ swift-yaml/YAML.swift
@@ -0,0 +1,17 @@
+//
+//  YAML.swift
+//  swift-yaml
+//
+//  Created by Niels de Hoog on 15/07/15.
+//  Copyright © 2015 Invisible Pixel. All rights reserved.
+//
+
+import Foundation
+
+
+public struct YAML {
+    static func load(string: String) {
+        let parser: UnsafeMutablePointer<yaml_parser_t>?
+        
+    }
+}
